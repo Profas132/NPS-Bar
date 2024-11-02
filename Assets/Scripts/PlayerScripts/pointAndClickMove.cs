85ab07c63b47c2747a697c654da4ba9634c78443
SvinPepe <2007nik.syc@gmail.com>
2024-11-02 14:25:30 +0500

    Передвижение перса

--- Assets/Scripts/PlayerScripts/pointAndClickMove.cs
+++ Assets/Scripts/PlayerScripts/pointAndClickMove.cs
@@ -1,27 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class pointAndClickMove : MonoBehaviour
 {
     public Vector2 mousePosition;
     public Vector2 targetPosition;
-    [SerializeField][Range(0,10)] private float speed;
+    [SerializeField][Range(0, 0.1f)] private float speed;
 
-    private void Update()
+    private void FixedUpdate()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector2 targetPosition = mousePosition;
-            if (this.transform.position.x != targetPosition.x)
-            {
-                this.transform.position = Vector2.Lerp(this.transform.position, targetPosition, speed * Time.deltaTime);
-            }
-            //
-            //transform.Translate(targetPosition);        
+            targetPosition = mousePosition;
+            Debug.Log(targetPosition);
         }
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed);
     }
 }
