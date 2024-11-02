63d7edf5a7d441907ce514aba867a8f282a03def
Profas132 <113793592+Profas132@users.noreply.github.com>
2024-11-02 09:15:27 +0300

    Добавление деталей интерфейса

---
+++ Assets/Scripts/PlayerScripts/pointAndClickMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class pointAndClickMove : MonoBehaviour
+{
+    public Vector2 mousePosition;
+    public Vector2 targetPosition;
+    [SerializeField][Range(0,10)] private float speed;
+
+    private void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 targetPosition = mousePosition;
+            if (this.transform.position.x != targetPosition.x)
+            {
+                this.transform.position = Vector2.Lerp(this.transform.position, targetPosition, speed * Time.deltaTime);
+            }
+            //
+            //transform.Translate(targetPosition);        
+        }
+    }
+}
