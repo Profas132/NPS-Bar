556188bc6e2c1e423daaf25be436a9d1fbc83493
SvinPepe <2007nik.syc@gmail.com>
2024-11-03 17:33:16 +0500

    сценарий движения персонажей

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,18 +8,17 @@ public class EnemyMovement : MonoBehaviour
 
     static public Transform outdoor;
     static public Transform barStage;
-    [SerializeField] float enemySpeed = 0.1f;
+    [SerializeField] float enemySpeed = 0.01f;
 
     public Coroutine GoToTable()
     {        
-        return StartCoroutine(nameof(goToTable));
+        return StartCoroutine(goToTable());
     }
 
-    private IEnumerable goToTable()
+    private IEnumerator goToTable()
     {
         
-        while (Vector2.Distance(transform.position, barStage.position) > float.Epsilon) {
-            Debug.Log("Xd");
+        while (Vector2.Distance(transform.position, barStage.position) > 0.01f) {            
             transform.position = Vector2.MoveTowards(transform.position, barStage.position, enemySpeed);
             yield return null;
         }
@@ -31,9 +30,9 @@ public class EnemyMovement : MonoBehaviour
         return StartCoroutine(nameof(goOut));
     }
 
-    private IEnumerable goOut()
+    private IEnumerator goOut()
     {
-        while (Vector2.Distance(transform.position, outdoor.position) > float.Epsilon)
+        while (Vector2.Distance(transform.position, outdoor.position) > 0.01f)
         {
             transform.position = Vector2.MoveTowards(transform.position, outdoor.position, enemySpeed);
             yield return null;
