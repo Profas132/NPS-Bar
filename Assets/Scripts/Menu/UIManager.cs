804092b247ba743481a79411d9ad08ad574989f8
SvinPepe <2007nik.syc@gmail.com>
2024-11-03 22:46:02 +0500

    + настройка аудио в меню

--- Assets/Scripts/Menu/UIManager.cs
+++ Assets/Scripts/Menu/UIManager.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    [SerializeField] TMP_Text soundValueLabel;
+    [SerializeField] Slider slider;
+    
+
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("SoundValue", 50f) / 100;
+
+
+
+    }
+
     public void OnStartClick()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void SliderValueChanged()
+    {
+        soundValueLabel.text = Mathf.Floor(slider.value * 100f).ToString();
+        PlayerPrefs.SetFloat("SoundValue", Mathf.Floor(slider.value * 100f));
     }
 }
